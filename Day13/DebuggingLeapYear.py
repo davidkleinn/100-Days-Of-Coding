93e0e9dcb0caf426fce37fd677eead6436d8b0e6
Davi Monte Klein <davimonteklein1711@gmail.com>
2025-10-14 21:23:21 -0300

    python coding projects

---
+++ Day13/DebuggingLeapYear.py
@@ -0,0 +1,30 @@
+# ORIGINAL CODE:
+#
+# def is_leap(year):
+#    if year % 4 == 0:
+#        if year % 100 == 0:
+#            if year % 4000 == 0:
+#                return True
+#            else:
+#                return False
+#        else:
+#            return True
+#    else:
+#        return False
+
+
+# DEBUGGED: 
+
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+    
+print(is_leap(2020))
